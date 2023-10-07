ff7b3b3f30ae64a928607c5190f60e3abb0b9825
tim <timothy.lawless@gmail.com>
2023-10-07 13:34:42 -0400

    added more

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "log"
+import (
+	"log"
+	"sort"
+)
 
 func main() {
 	log.Println("Begin...")
@@ -20,4 +23,44 @@ func main() {
 
 	log.Println(myMap["dog"])
 	log.Println(myMap["odog"])
+	log.Println("================================")
+	myMap2 := make(map[string]int)
+	myMap2["First"] = 6
+	myMap2["Second"] = 3
+	log.Println(myMap2["First"], "Plus", myMap2["Second"], "Equals: ", myMap2["First"]+myMap2["Second"])
+
+	type User struct {
+		FirstName string
+		LastName  string
+	}
+	myMap3 := make(map[string]User)
+	me := User{
+		FirstName: "Tim",
+		LastName:  "Law",
+	}
+	myMap3["me"] = me
+	log.Println(myMap3["me"].FirstName)
+	log.Println(myMap3["me"].LastName)
+
+	var mySlice []string
+	mySlice = append(mySlice, "Florida")
+	mySlice = append(mySlice, "Georgia")
+	mySlice = append(mySlice, "Texas")
+	mySlice = append(mySlice, "Utah")
+	mySlice = append(mySlice, "New Jersey")
+
+	log.Println(mySlice)
+
+	var mySlice2 []int
+
+	mySlice2 = append(mySlice2, 5)
+	mySlice2 = append(mySlice2, 6)
+	mySlice2 = append(mySlice2, 7)
+	mySlice2 = append(mySlice2, 0)
+	mySlice2 = append(mySlice2, 9)
+
+	log.Println(mySlice2)
+	sort.Ints(mySlice2)
+	log.Println(mySlice2)
+
 }
